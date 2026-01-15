8d94b58c0109d4ba5d516ae73d2cf97a3dd9d3d2
Khaderik <hrdcedric@gmail.com>
2026-01-15 22:57:15 +0100

    fix(config): remove trackball_dpi.h and update mouse layer bindings
    The trackball_dpi.h file has been removed and the mouse layer bindings in keyball39.keymap have been updated to replace the trackball controls with transparent keys.

--- config/trackball_dpi.h
+++
@@ -1,7 +0,0 @@
-#ifndef PMW3610_KEYCODES_H
-#define PMW3610_KEYCODES_H
-
-#define PMW3610_CPI_UP     0x12E6    /* Increase trackball CPI */
-#define PMW3610_CPI_DOWN   0x12E7   /* Decrease trackball CPI */
-
-#endif
